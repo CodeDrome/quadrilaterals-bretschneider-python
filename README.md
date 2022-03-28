b87cb61b7548de944f9c933c8b48ea439c548c90
Chris Webb <50113303+CodeDrome@users.noreply.github.com>
2022-03-28 15:39:30 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+For full details of this project go to
+
+https://www.codedrome.com/quadrilaterals-areas-bretschneiders-formula-python
---
+++ bretschneidersformula.py
@@ -0,0 +1,30 @@
+from functools import reduce
+import math
+
+
+def calculate_area(sides, opposite_angles_degrees):
+
+    '''
+    Calculate the area of a quadrilateral using Bretschneider's Formula
+    from the lengths of the sides and either pair of OPPOSITE angles.
+    Arguments:-
+    sides - list in format [a,b,c,d]
+    opposite_angles_degrees - list in format [a1,a2]
+    '''
+
+    semiperimeter = sum(sides) / 2
+
+    product_semiperimeter_minus_sides = (semiperimeter - sides[0]) * \
+                                        (semiperimeter - sides[1]) * \
+                                        (semiperimeter - sides[2]) * \
+                                        (semiperimeter - sides[3])
+
+    product_sides = reduce((lambda x, y: x * y), sides)
+
+    sum_of_radians = math.radians(opposite_angles_degrees[0]) + math.radians(opposite_angles_degrees[1])
+
+    cos_squared = ( math.cos(sum_of_radians / 2 ) ) **2
+
+    area = math.sqrt(product_semiperimeter_minus_sides - product_sides * cos_squared)
+
+    return area
